65021b898d63cf7981b04b651e193b8ddd395373
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-03-17 00:29:12 +0600

    add 2 more view classes of app 'groups'

--- social_site/groups/views.py
+++ social_site/groups/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 from django.urls import reverse
+from django.contrib import messages
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from .models import Group, GroupMember
 # Create your views here.
@@ -16,3 +18,39 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug'})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except InterruptedError:
+            messages.warning(self.request, 'Warning already a member')
+        else:
+            messages.success(self.request, 'You are now member')
+
+        return super().get(, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug'})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug = self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'SOrry, yo are not in this group')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have successfully leave the group')
+        return super().get(, *args, **kwargs)
