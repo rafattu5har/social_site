860b0628aff8a5fdcc71e3e5e151f21c451e135d
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-03-20 14:25:15 +0600

    add comment model in 'posts' app

--- social_site/posts/models.py
+++ social_site/posts/models.py
@@ -13,7 +13,7 @@ User = get_user_model()
 
 class Post(models.Model):
     user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
     group = models.ForeignKey(Group, related_name='posts', null=True, blank=True, on_delete=models.CASCADE)
@@ -32,3 +32,21 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created_at']
         unique_together = ['user', 'message']
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('posts:all')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-created_at']
