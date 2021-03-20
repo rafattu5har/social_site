9cd7f80b79dd1224705186129916e8ac6b7a28f6
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-03-20 12:54:10 +0600

    update url with new edit views in 'posts' app

--- social_site/posts/urls.py
+++ social_site/posts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^by/(?P<username>[-\w]+)/$', views.UserPost.as_view(), name='for_user'),
     url(r'^by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
     url(r'^delete/(?P<pk>\d+)/$', views.DeletePost.as_view(), name='delete'),
+    url(r'^edit/(?P<pk>\d+)/$', views.EditPost.as_view(), name='edit'),
 ]
