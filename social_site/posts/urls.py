0801251272e8b555c3de78995faac58a69db78e7
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-03-24 01:33:09 +0600

    adding post_detail FBV in urls in app 'posts'

--- social_site/posts/urls.py
+++ social_site/posts/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('',views.PostList.as_view(),name='all'),
     path('new/', views.CreatePost.as_view(), name='create'),
     url(r'^by/(?P<username>[-\w]+)/$', views.UserPost.as_view(), name='for_user'),
-    url(r'^by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
+    # url(r'^by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
     url(r'^delete/(?P<pk>\d+)/$', views.DeletePost.as_view(), name='delete'),
     url(r'^edit/(?P<pk>\d+)/$', views.EditPost.as_view(), name='edit'),
     url(r'^comment/(?P<pk>\d+)/$', views.CreateComment.as_view(), name='create_comment'),
     url(r'^comment/delete/(?P<pk>\d+)/$', views.DeleteComment.as_view(), name='delete_comment'),
+    url(r'^test/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.post_detail, name='single'),
 ]
